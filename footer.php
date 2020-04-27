20b37aa3624aec263b201630249fed89457a100f
surbhi2101 <gangrade.surbhi@yahoo.com>
2020-04-27 21:46:41 +0530

    Restaurent site

---
+++ footer.php
@@ -0,0 +1,17 @@
+<div class="footer1">
+<div class="row">
+<div class="col-md-5 col-md-offset-1">
+<h4><b>Develped By:-</b></h4>
+<p>Vaibhav Gangrade</p>
+<p>Surbhi Gangrade</p>
+</div>
+<div class="col-md-5 col-md-offset-1">
+<h4><b>Mobile Number:-</b></h4>
+<p>8109728281</p
+<p>8224022245</p>
+</div>
+</div> 
+<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
---
+++ headder.php
@@ -0,0 +1,39 @@
+<! DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Product</title>
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/animate.css">
+</head>
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+	  
+      <a class="navbar-brand " href="index.html">
+	 <div class="mylogo">
+	 <img src="img/restologo.png" height="35px" width="100px" />
+    </div></a>
+	
+	</div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+	   <li><a href="index.html"><span style="color:white">Home</span></a></li>
+		<li><a href="about.html"><span style="color:white">About</span> </a></li>
+        <li><a href="contact.html"><span style="color:white">Contact</span></a></li>
+       
+            <li><a href="product.html"><span style="color:white">products</span></a></li>
+			
+        <li><a href="register.html"><span style="color:white"> Register</span> </a></li>
+        <li><a href="login.html"> <span style="color:white">Login</span></a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
